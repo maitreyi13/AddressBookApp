ad0e7d193a7cb74c2375068887f12d9150a1dcba
maitreyi13 <kulkarnimaitreyi0@gmail.com>
2022-10-04 18:35:01 +0530

    service class for address book

---
+++ src/main/java/com/example/addressbookapp/service/AddressBookService.java
@@ -0,0 +1,56 @@
+package com.example.addressbookapp.service;
+import com.example.addressbookapp.dto.ContactDTO;
+import com.example.addressbookapp.exception.AddressBookException;
+import com.example.addressbookapp.model.Contact;
+import com.example.addressbookapp.repo.Repo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AddressBookService implements IAddressBookService {
+    @Autowired
+    Repo repository;
+    @Override
+    public Contact createContact(ContactDTO contactDTO) {
+        Contact newContactData = new Contact(contactDTO);
+        return repository.save(newContactData);
+    }
+    @Override
+    public Optional<Contact> getContactById(Long contactId) {
+        if (repository.findById(contactId).isPresent()) {
+            return repository.findById(contactId);
+        } else throw new AddressBookException("No contact in database.");
+    }
+    @Override
+    public List<Contact> getContact() {
+        if (repository.findAll().isEmpty())
+        {
+            System.out.println("No data found");
+            throw new AddressBookException("No contact in database.");
+
+        } else return repository.findAll();
+    }
+    @Override
+    public Contact updateContact(Long contactId, ContactDTO contactDTO) {
+        Contact existingGreet = repository.findById(contactId).orElse(null);
+        if (existingGreet != null) {
+            existingGreet.setFirstName(contactDTO.getFirstName());
+            existingGreet.setLastName(contactDTO.getLastName());
+            existingGreet.setAddress(contactDTO.getAddress());
+            existingGreet.setState(contactDTO.getState());
+            existingGreet.setCity(contactDTO.getCity());
+            existingGreet.setZip(contactDTO.getZip());
+            existingGreet.setPhone(contactDTO.getPhone());
+        }
+        assert existingGreet != null;
+        return repository.save(existingGreet);
+    }
+
+    @Override
+    public void deleteContact(Long contactId) {
+        repository.deleteById(contactId);
+    }
+}
---
+++ src/main/java/com/example/addressbookapp/service/IAddressBookService.java
@@ -0,0 +1,18 @@
+package com.example.addressbookapp.service;
+import com.example.addressbookapp.dto.ContactDTO;
+import com.example.addressbookapp.model.Contact;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IAddressBookService {
+    List<Contact> getContact();
+
+    Contact updateContact(Long contactId, ContactDTO contactDTO);
+
+    Contact createContact(ContactDTO contactDTO);
+
+    Optional<Contact> getContactById(Long contactId);
+    void deleteContact(Long contactId);
+
+}
